84a0b06b3e7de44f3fb6a74968ebd90d0817c586
Daniils <daniils.miscuks@gamail.com>
2023-04-30 03:39:30 +0300

    Izdariju Sakum Anian, Izdariju intertars ar item,..

---
+++ Game/Assets/Script/StartGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using System.Threading;
+public class StartGame : MonoBehaviour {
+
+    public void Uzaskums()
+    {
+      
+        SceneManager.LoadScene("Sakums", LoadSceneMode.Single);
+    }
+
+    //Metode,kura aizver programmu, ja ta ir izveidota ka .exe
+
+    public void Apturet()
+    {
+		Debug.Log ("Quit is working");
+        Application.Quit();
+    }
+
+    //Pasiem jauztaisa metode,kas parsledzno sakuma uz UI ainu
+    public void Game()
+    {
+		Thread.Sleep(1000);
+        SceneManager.LoadScene("CreatePers", LoadSceneMode.Additive);
+
+    }
+}
