f11dd82bc689c04ff28c20fa0e5e37fe006c52a3
Daniils <daniils.miscuks@gamail.com>
2023-05-02 23:51:28 +0300

    Save to LVT class

---
+++ Game/Assets/Script/IklButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IklButton : MonoBehaviour {
+
+
+
+    public GameObject BlueOneImg;
+    public GameObject GreenOneImg;
+
+
+    public void Uzkl(bool vertiba)
+    {
+        BlueOneImg.SetActive(vertiba);
+    }
+    public void Neuzkl(bool vertiba)
+    {
+       GreenOneImg.SetActive(vertiba);
+    }
+
+}
