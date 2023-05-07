756f266f0c5f7b22841f419976e9dab9d008b4aa
Daniils <daniils.miscuks@gamail.com>
2023-05-08 00:34:14 +0300

    Izdariju text(Ne lidz galam) unnn vel kaut ko idatiju, ir loti daudz jautajumi pie skolotajam, ne sapratu daudz ko, ir problemas ar apgerbie(RENDER

---
+++ Game/Assets/Script/MainitLiel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainitLiel : MonoBehaviour {
+
+
+    public GameObject mainigaisAttles;
+    public Sprite[] atteluMasiva;
+    public GameObject xSlaiders;
+    public GameObject ySlaiders;
+
+
+
+    public void izkritosais(int skaitlis)
+    {
+        if (skaitlis == 0)
+            mainigaisAttles.GetComponent<Image>().sprite = atteluMasiva[0];
+        else if (skaitlis == 1)
+            mainigaisAttles.GetComponent<Image>().sprite = atteluMasiva[1];
+        else if (skaitlis == 2)
+            mainigaisAttles.GetComponent<Image>().sprite = atteluMasiva[2];
+        else Debug.Log("Nav piesaistits attels!");
+    }
+
+
+
+
+
+    public void mainitLielumu()
+    {
+        float pasreizajaVertiba = xSlaiders.GetComponent<Slider>().value;
+        mainigaisAttles.transform.localScale = new Vector2(1f * pasreizajaVertiba, 0);
+    }
+    public void mainiRotaciju()
+    {
+        float pasreizajaVertiba = ySlaiders.GetComponent<Slider>().value;
+        mainigaisAttles.transform.localScale = new Vector2(0, 1f * pasreizajaVertiba);
+    }
+}
--- Game/Assets/Script/StartGame.cs
+++ Game/Assets/Script/StartGame.cs
@@ -1,14 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using System;
+using UnityEngine.UI;
 using System.Threading;
+using UnityEngine.SceneManagement;
+
+
 public class StartGame : MonoBehaviour {
+    private string Vards;
+    private int vec;
+    public GameObject ievadesLauksText;
+    public GameObject ievadesLauksVec;
+    public GameObject tekstaAttelosana;
+    private string[] fragmentiVec = { "Years", "Gadi", "Godikov"};
+    private string[] fragmenti = { "Viss stiprakais ", " Luzers ", "'PRO99LVL'"};
+    int index;
+    public void UzglabatTekstu()
+    {
+      
+      
+            index = Random.Range(0, fragmenti.Length);
+            Vards = ievadesLauksText.GetComponent<InputField>().text;
+       tekstaAttelosana.GetComponent<Text>().text = fragmenti[index] + Vards.ToUpper() + "!";
+
+
+    }
+    public void UzglabatVecums()
+    {
+        vec = int.Parse(ievadesLauksVec.GetComponent<InputField>().text);
+        tekstaAttelosana.GetComponent<Text>().text = fragmentiVec[index] + " " + vec + " " + fragmenti[index];
+
+    }
+
 
     public void Uzaskums()
     {
-      
+
         SceneManager.LoadScene("Sakums", LoadSceneMode.Single);
     }
 
@@ -16,15 +43,17 @@ public class StartGame : MonoBehaviour {
 
     public void Apturet()
     {
-		Debug.Log ("Quit is working");
+        Debug.Log("Quit is working");
         Application.Quit();
     }
 
     //Pasiem jauztaisa metode,kas parsledzno sakuma uz UI ainu
     public void Game()
     {
-		Thread.Sleep(1000);
+        Thread.Sleep(1000);
         SceneManager.LoadScene("CreatePers", LoadSceneMode.Additive);
 
     }
+
+
 }
