f81092020e94b8039d948827a73d86eb7781bf9d
Daniils <daniils.miscuks@gamail.com>
2023-05-15 19:39:54 +0300

    Add things

---
+++ Game/Assets/Script/ChangeImage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ChangeImage : MonoBehaviour {
+    public Sprite[] atteluMasivs;
+    public GameObject MAttles;
+
+    public void izkritosais(int skaitlis)
+    {
+        if (skaitlis == 0)
+            MAttles.GetComponent<Image>().sprite = atteluMasivs[0];
+        else if (skaitlis == 1)
+            MAttles.GetComponent<Image>().sprite = atteluMasivs[1];
+        else if (skaitlis == 2)
+            MAttles.GetComponent<Image>().sprite = atteluMasivs[2];
+        else if (skaitlis == 3)
+            MAttles.GetComponent<Image>().sprite = atteluMasivs[3];
+        else if (skaitlis == 4)
+            MAttles.GetComponent<Image>().sprite = atteluMasivs[4];
+        else Debug.Log("Nav piesaistits attels!");
+    }
+}
